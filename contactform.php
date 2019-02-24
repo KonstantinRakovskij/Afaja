31380174f0473cad939ef021295864bbd3fcd182
KonstantinRakovskij <34558479+KonstantinRakovskij@users.noreply.github.com>
2019-02-24 18:31:40 +0100

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -11,6 +11,6 @@ if (isset ($_POST['submit'])){
     $txt = "You have recieve an e-mail from".$name. ".\n\n".$message;
 
     mail($mailTo,$subject,$txt,$headers);
-    header("Location: contactus.php?mailsend")
+    header("Location: contactus.html?mailsend")
 }
 ?>
